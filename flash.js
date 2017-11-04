6b2006a93feee598d7bde69ca8c3eef4827a7e58
Nick Wanninger <nickwanninger@gmail.com>
2017-11-03 23:05:43 -0500

    release

--- flash.js
+++ flash.js
@@ -3,28 +3,38 @@ const { spawn } = require('child_process')
 const fs = require('fs')
 const leftPad = require('left-pad')
 
-const message = 'hello world'
+const program = require('commander')
+program.parse(process.argv)
 
-const chars = message.split('')
-const delay = 50
-let bits = []
-chars.forEach(c => {
-	const n = c.charCodeAt(0)
-	const binaryString = n.toString('2')
-	const binaryList = leftPad(binaryString, 8, '0').split('')
-	bits = [ ...bits, ...binaryList ]
-})
+const message = program.args[0]
 
-const flash = b => {
-	const s = b == 0 ? '-' : '+'
-	spawn('./setleds', [ `${s}caps` ])
-}
-console.log(bits[bits.length - 1])
-bits.forEach((b, i) => {
-	setTimeout(() => flash(b), i * delay)
-	if (i == bits.length) {
-		setTimeout(() => {
-			flash(0)
-		}, i * delay + delay)
+if (message != null) {
+	console.log('flashing ' + message + ' to your capslock LED')
+	const chars = message.split('')
+	const delay = 50
+
+	let bits = []
+	chars.forEach(c => {
+		const n = c.charCodeAt(0)
+		const binaryString = n.toString('2')
+		const binaryList = leftPad(binaryString, 8, '0').split('')
+		bits = [ ...bits, ...binaryList ]
+	})
+
+	const flash = b => {
+		const s = b == 0 ? '-' : '+'
+		spawn('./setleds', [ `${s}caps` ])
 	}
-})
+
+	bits.forEach((b, i) => {
+		setTimeout(() => flash(b), i * delay)
+		if (i == bits.length) {
+			setTimeout(() => {
+				flash(0)
+				console.log('done flashing')
+			}, i * delay + delay)
+		}
+	})
+} else {
+	console.log('please enter a message')
+}
